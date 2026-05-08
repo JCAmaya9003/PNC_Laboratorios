1cc74974a0641cdf017a421434ee2de3408064e5
JCAmaya9003 <00365123@uca.edu.sv>
2026-05-07 21:19:59 -0600

    :rocket: add: Agregando configuracion YAML y modelo

---
+++ Labo02/src/main/java/org/example/labo02/domain/entity/Pirate.java
@@ -0,0 +1,33 @@
+package org.example.labo02.domain.entity;
+
+import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+import java.util.UUID;
+
+@Entity
+@Table(name = "Pirate")
+@Data
+@Builder
+@AllArgsConstructor
+@NoArgsConstructor
+public class Pirate {
+    @Id
+    @GeneratedValue(strategy = GenerationType.UUID)
+    private UUID id;
+
+    @Column(name = "name")
+    private String name;
+
+    @Column(name = "bounty")
+    private Double bounty;
+
+    @Column(name = "crew")
+    private String crew;
+
+    @Column(name = "isALive")
+    private Boolean isALive;
+}
